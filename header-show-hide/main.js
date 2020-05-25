0c7a61796302cb26e240f6425f98e778f1981966
Abhinav R <coralnodes@gmail.com>
2020-05-26 05:06:42 +0530

    hide show header

---
+++ header-show-hide/main.js
@@ -0,0 +1,75 @@
+(function(){
+
+    var doc = document.documentElement;
+    var w   = window;
+
+    /*
+    define four variables: curScroll, prevScroll, curDirection, prevDirection
+    */
+
+    var curScroll;
+    var prevScroll = w.scrollY || doc.scrollTop;
+    var curDirection = 0;
+    var prevDirection = 0;
+
+    /*
+    how it works:
+    -------------
+    create function to check scroll postion on each scroll event,
+    compare curScroll and prevScroll values to find the scroll direction
+    scroll up - 1, scroll down - 2, initial - 0
+
+    then set the direction value to curDirection
+
+    compare curDirection and prevDirection
+
+    if it is different, call a function to show or hide the header
+
+    example:
+    step 1: user scrolls down: curDirection 2, prevDirection 0 > hide header
+    step 2: user scrolls down again: curDirection 2, prevDirection 2 > already hidden, do nothing
+    step 3: user scrolls up: curDirection 1, prevDirection 2 > show header
+    */
+
+    var header = document.getElementById('site-header');
+    var toggled;
+    var threshold = 200;
+
+    var checkScroll = function() {
+        curScroll = w.scrollY || doc.scrollTop;
+        if(curScroll > prevScroll) {
+            // scrolled down
+            curDirection = 2;
+        }
+        else if (curScroll < prevScroll) {
+            //scrolled up
+            curDirection = 1;
+        }
+
+        if(curDirection !== prevDirection) {
+            toggled = toggleHeader();
+        }
+
+        prevScroll = curScroll;
+        if(toggled) {
+            prevDirection = curDirection;
+        }
+    };
+
+    var toggleHeader = function() { 
+        toggled = true;
+        if(curDirection === 2 && curScroll > threshold) {
+            header.classList.add('hide');
+        }
+        else if (curDirection === 1) {
+            header.classList.remove('hide');
+        }
+        else {
+            toggled = false;
+        }
+        return toggled;
+    };
+
+    window.addEventListener('scroll', checkScroll);
+
+})();
