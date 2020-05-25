c11708fb5619fe03523e3d0ae024cd98e768e09f
Abhinav R <coralnodes@gmail.com>
2020-05-25 08:45:47 +0530

    file changed

---
+++ php-parse-html/README.md
@@ -0,0 +1,9 @@
+# Topics in this tutorial
+
+- selecting elements by id
+- selecting elements using tagname
+- selecting elements using xpath query
+- create and insert new elements into the page
+- delete an element from the page
+- getting element attribute values
+- setting attribute values
--- php-parse-html/shop.php
+++ php-parse-html/shop.php
@@ -10,34 +10,21 @@ $html = ob_get_clean();
 ** parse html before outputting or saving
 */
 
-$dom = new DOMDocument();
-@ $dom->loadHTML($html, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
-
 // 1. inserting a new element into the document
 // eg., find the no. of products and show it at the top
+
+$dom = new DOMDocument();
+@ $dom->loadHTML($html, LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
 
 $xpath = new DOMXPath($dom);
 $products = $xpath->query("//div[@id='product-list']/div"); //css equivalent to div#product-list > div
 $count = $products->length;
-
-// insert the number above the list
 
 $product_list = $dom->getElementById('product-list');
 
 $new_element = $dom->createElement('div', 'No. of products: ' . $count);
 $new_element->setAttribute('class', 'alert alert-info');
 $product_list->parentNode->insertBefore($new_element, $product_list);
-
-/* alternate method
-$new_dom = new DOMDocument();
-@ $new_dom->loadHTML('<div class="count">No. of products: '. $count .'</div>', LIBXML_HTML_NODEFDTD | LIBXML_HTML_NOIMPLIED);
-
-$new_node = $new_dom->documentElement;
-
-$imported_node = $dom->importNode($new_node, true);
-
-$product_list->parentNode->insertBefore($imported_node, $product_list);
-*/
 
 // 2. delete an element from the document
 
@@ -47,7 +34,10 @@ $title_el = $title->item(0);
 
 $title_el->parentNode->removeChild($title_el);
 
-// finally, save & output the html
+// save the new html
 
 $html = @ $dom->saveHTML();
+
+// finally, output the html
+
 echo $html;
